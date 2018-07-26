b8cd18a16cf80602c9fa3048058b5f3e0afe2a72
newyaso <newyasso@gmail.com>
2018-07-26 23:37:40 +0200

    Task_1_loginActivity

--- app/src/main/java/de/rheingold/view/LoginActivity.java
+++ app/src/main/java/de/rheingold/view/LoginActivity.java
@@ -1,5 +1,40 @@
 package de.rheingold.view;
 
-public class LoginActivity
-{
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    SharedPreferences sharedPref;
+    private EditText etEmail;
+    private EditText etPass;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.btnLogin);
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPass);
+        login.setOnClickListener(this);
+        sharedPref = this.getSharedPreferences("SharedPreference_userInfo", MODE_PRIVATE);
+        etEmail.setText(sharedPref.getString("Email Adress", ""));
+        etPass.setText(sharedPref.getString("Paswwort", ""));
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("Email Adress", etEmail.getText().toString());
+        editor.putString("Paswwort", etPass.getText().toString());
+        editor.apply();
+
+    }
 }
